b2ba599207bfab935426446c380ef79ef16e28cc
Pablo Cortez <57056367+cspablocortez@users.noreply.github.com>
2025-03-29 15:53:43 -0700

    Add scripts

---
+++ minify.js
@@ -0,0 +1,27 @@
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function minifyCSS(css) {
+  return css
+    .replace(/\/\*(?:(?!\*\/)[\s\S])*\*\/|[\r\n\t]+/g, "") // Remove comments and whitespace
+    .replace(/ {2,}/g, " ") // Remove multiple spaces
+    .replace(/ ([{:}]) /g, "$1") // Remove spaces around brackets and colons
+    .replace(/([;:,]) /g, "$1") // Remove spaces after semicolons, colons and commas
+    .replace(/ !/g, "!"); // Remove spaces before important
+}
+
+const inputFile = path.join(__dirname, "cosmo.css");
+const outputFile = path.join(__dirname, "cosmo.min.css");
+
+try {
+  const data = await fs.readFile(inputFile, "utf8");
+  const minifiedCSS = minifyCSS(data);
+  await fs.writeFile(outputFile, minifiedCSS, "utf8");
+  console.log("CSS minified successfully!");
+} catch (err) {
+  console.error("Error:", err);
+}
